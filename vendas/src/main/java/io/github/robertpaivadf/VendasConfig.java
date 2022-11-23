e1c95d3981a4e97fa8d8dbe62e1ba5a1d1724fee
robertpaivadf <robertpaiva.df@gmail.com>
2022-11-23 07:18:16 -0300

    Utilizando profiles e CommandLineRunner

--- vendas/src/main/java/io/github/robertpaivadf/VendasConfig.java
+++ vendas/src/main/java/io/github/robertpaivadf/VendasConfig.java
@@ -1,7 +1,9 @@
 package io.github.robertpaivadf;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class VendasConfig {
@@ -10,4 +12,13 @@ public class VendasConfig {
     public String applicationName(){
         return "Sistema de Vendas";
     }
+
+    @Bean
+    @Profile("dev")
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("Executando o profile dev");
+        };
+    }
+
 }
