1704e150e47a890edf31f6b956a318c290a45280
robertpaivadf <robertpaiva.df@gmail.com>
2022-11-23 15:37:28 -0300

    Reiniciando o projeto vendas

--- vendas/src/main/java/io/github/robertpaivadf/Animal.java
+++
@@ -1,5 +0,0 @@
-package io.github.robertpaivadf;
-
-public interface Animal {
-    void fazerBarulho();
-}
--- vendas/src/main/java/io/github/robertpaivadf/VendasApplication.java
+++ vendas/src/main/java/io/github/robertpaivadf/VendasApplication.java
@@ -1,13 +1,10 @@
 package io.github.robertpaivadf;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +16,6 @@ public class VendasApplication {
     private String applicationName;
 
     //Teste animal
-    @Cachorro
     private Animal animal;
 
     @Bean
