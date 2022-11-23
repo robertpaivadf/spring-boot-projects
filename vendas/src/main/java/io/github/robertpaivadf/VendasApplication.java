3336be8c049fc7879d26d851bcaca744face7a88
robertpaivadf <robertpaiva.df@gmail.com>
2022-11-23 06:58:22 -0300

    Configurações application.properties

--- vendas/src/main/java/io/github/robertpaivadf/VendasApplication.java
+++ vendas/src/main/java/io/github/robertpaivadf/VendasApplication.java
@@ -2,6 +2,7 @@ package io.github.robertpaivadf;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -12,8 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VendasApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
+    @Value("${application.name}")
     private String applicationName;
 
     @GetMapping("/teste")
