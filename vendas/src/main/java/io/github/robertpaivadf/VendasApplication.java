048df9abe61ee2df4c016770fc88f2189d2cae3c
robertpaivadf <robertpaiva.df@gmail.com>
2023-01-03 00:03:01 -0300

    Criando controller para buscar cliente pelo id

--- vendas/src/main/java/io/github/robertpaivadf/VendasApplication.java
+++ vendas/src/main/java/io/github/robertpaivadf/VendasApplication.java
@@ -1,10 +1,24 @@
 package io.github.robertpaivadf;
 
+import io.github.robertpaivadf.domain.entities.Cliente;
+import io.github.robertpaivadf.domain.repositories.RepCliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired RepCliente repCliente){
+        return args ->{
+            Cliente c = new Cliente(null, "Robert");
+            repCliente.save(c);
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
--- vendas/src/main/java/io/github/robertpaivadf/rest/controller/ClienteController.java
+++ vendas/src/main/java/io/github/robertpaivadf/rest/controller/ClienteController.java
@@ -1,17 +1,32 @@
 package io.github.robertpaivadf.rest.controller;
 
+import io.github.robertpaivadf.domain.entities.Cliente;
+import io.github.robertpaivadf.domain.repositories.RepCliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    //@RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
-    @GetMapping (value = "/hello/{nome}")
+    @Autowired
+    RepCliente repCliente;
+
+    @GetMapping(value = "/{id}")
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente){
-        return String.format("Hello %s", nomeCliente);
+    public ResponseEntity getClienteById(@PathVariable Integer id) {
+        Optional<Cliente> cliente = repCliente.findById(id);
+        if(cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
